cb62e55db7dfb3b2281104bbd82549f57fec3e42
David Ghazi <davidaghazi@gmail.com>
2020-04-06 19:13:22 -0400

    fixed the navbar and view work button

---
+++ script.js
@@ -0,0 +1,22 @@
+$('.view').click(function(){
+    $('html, body').animate({
+        scrollTop: ($('.navbar-dark').offset().top)
+    },500);
+});
+
+$(document).ready(function () {  
+    var top = $('.navbar-dark').offset().top - parseFloat($('.navbar-dark').css('marginTop').replace(/auto/, 100));
+    $(window).scroll(function (event) {
+      // what the y position of the scroll is
+      var y = $(this).scrollTop();
+  
+      // whether that's below the form
+      if (y >= top) {
+        // if so, ad the fixed class
+        $('.navbar-dark').addClass('fixed');
+      } else {
+        // otherwise remove it
+        $('.navbar-dark').removeClass('fixed');
+      }
+    });
+  });
