266790799551b095a93f2902c402593be3e4ed78
David Ghazi <davidaghazi@gmail.com>
2020-04-06 23:56:00 -0400

    added scroll to the page

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ $('.view').click(function(){
     },500);
 });
 
+$('.home-button').click(function(){
+    $('html, body').animate({
+        scrollTop: ($('body').offset().top)
+    },500);
+});
+
+$('.about-button').click(function(){
+    $('html, body').animate({
+        scrollTop: ($('.about').offset().top)
+    },500);
+});
+
+$('.portfolio-button').click(function(){
+    $('html, body').animate({
+        scrollTop: ($('.portfolio').offset().top)
+    },500);
+});
+
+$('.contact-button').click(function(){
+    $('html, body').animate({
+        scrollTop: ($('.contact').offset().top)
+    },500);
+});
+
 $(document).ready(function () {  
     var top = $('.navbar-dark').offset().top - parseFloat($('.navbar-dark').css('marginTop').replace(/auto/, 100));
     $(window).scroll(function (event) {
