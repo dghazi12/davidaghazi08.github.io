4038794f6cd62f2bafbb6732c548140411b88a12
David Ghazi <davidaghazi@gmail.com>
2020-04-07 23:47:06 -0400

    getting the buttons to redirect using jquery

--- script.js
+++ script.js
@@ -1,33 +1,49 @@
+//When you click view my work button, it will take you to the about section of the webpage
 $('.view').click(function(){
     $('html, body').animate({
         scrollTop: ($('.navbar-dark').offset().top)
     },500);
 });
 
+//When you click view my resume button, it will take you to a pdf file of my resume
+$(".cv").click(function() {
+    window.open("images/Full-Stack Resume-pages-1.pdf", "windowName");
+});
+
+//When you click home button, it will take you to the welcome section of the webpage
 $('.home-button').click(function(){
     $('html, body').animate({
         scrollTop: ($('body').offset().top)
     },500);
 });
 
+//When you click about button, it will take you to the about section of the webpage
 $('.about-button').click(function(){
     $('html, body').animate({
         scrollTop: ($('.about').offset().top)
     },500);
 });
 
+//When you click portfolio button, it will take you to the portfolio section of the webpage
 $('.portfolio-button').click(function(){
     $('html, body').animate({
         scrollTop: ($('.portfolio').offset().top)
     },500);
 });
 
+//When you click contact button, it will take you to the contact section of the webpage
 $('.contact-button').click(function(){
     $('html, body').animate({
-        scrollTop: ($('.contact').offset().top)
+        scrollTop: ($('.contact112').offset().top)
     },500);
 });
 
+$("#osas-github").click(function() {
+    window.open('https://github.com/leeroyp/OsasTracks.github.io', '_blank');
+});
+
+
+//Created a sitcky nav bar. When you scroll through the page, the navbar scrolls as well
 $(document).ready(function () {  
     var top = $('.navbar-dark').offset().top - parseFloat($('.navbar-dark').css('marginTop').replace(/auto/, 100));
     $(window).scroll(function (event) {
